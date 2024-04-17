354447e4d28f63fb5a642193cbb45cbf0babf42a
Ivan Dvylyuk <ivan.dvylyuk@gmail.com>
2024-04-17 19:23:26 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,12 +3,20 @@ helm_list=$(helm list -o json)
 if [ "$helm_list" = "[]" ]; then
     echo "No deployments found"
     echo "Setting color variable to blue"
-    echo "##vso[task.setvariable variable=color]blue
+    echo "##vso[task.setvariable variable=color]blue"
 else
     echo "Deployments found"
     deployment_name=$(kubectl get deploy -o jsonpath='{.items[0].metadata.name}')
     echo "Deployment name: $deployment_name"
-    color=$(kubectl get deployment/$deployment_name -o jsonpath='{.spec.template.spec.containers[0].env[?(@.name=="color")].value}')
-    echo "Color: $color"
-    echo "##vso[task.setvariable variable=color]$color"
+    deploymentColor=$(kubectl get deployment/$deployment_name -o jsonpath='{.spec.template.spec.containers[0].env[?(@.name=="color")].value}')
+    echo "Color: $deploymentColor"
+
+    # Logic to set color variable based on deploymentColor
+    if [ "$deploymentColor" = "blue" ]; then
+        echo "Setting color variable to green"
+        echo "##vso[task.setvariable variable=color]green"
+    else
+        echo "Setting color variable to blue"
+        echo "##vso[task.setvariable variable=color]blue"
+    fi
 fi
